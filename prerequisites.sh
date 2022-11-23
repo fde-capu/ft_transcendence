c6a920ba700e28b254e263619d5e9d378a2b75c6
Flávio Carrara <flavio@carrara.us>
2022-11-23 19:16:58 -0300

    Now disables docker.service beforehead. Next: build ftt_backend..?

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-do_apt_update="true";#XXX
+do_apt_update="true";#XXX must be true
 do_apt_upgrade="true";
 docker_clean_containers="true";
 docker_clean_images="true";
@@ -12,6 +12,7 @@ do_docker_stop()
 	systemctl --user stop docker 2> /dev/null;
 	sudo systemctl stop docker.service 2> /dev/null;
 	systemctl --user daemon-reload 2> /dev/null;
+	sudo systemctl disable --now docker.service docker.socket;
 	return 0;
 }
 
@@ -30,28 +31,27 @@ do_docker_stop()
 	sudo apt install -y docker-compose-plugin;
 
 # For docker-compose in rootless mode:
-# Source: https://docs.docker.com/engine/security/rootless
-		sudo apt install -y uidmap; # updates even if already installed.
+	sudo apt install -y uidmap; # updates even if already installed.
 # Script to check user subordinates:
-		subuid=`grep ^$(whoami): /etc/subuid | awk -F ":" '{print $3}'`;
-		[ "$subuid" -lt "65536" ] && echo "Subordinate uid $subuid fail." && exit 1;
-		subgid=`grep ^$(whoami): /etc/subgid | awk -F ":" '{print $3}'`;
-		[ "$subgid" -lt "65536" ] && echo "Subordinate gid $subgid fail." && exit 2;
+	subuid=`grep ^$(whoami): /etc/subuid | awk -F ":" '{print $3}'`;
+	[ "$subuid" -lt "65536" ] && echo "Subordinate uid $subuid fail." && exit 1;
+	subgid=`grep ^$(whoami): /etc/subgid | awk -F ":" '{print $3}'`;
+	[ "$subgid" -lt "65536" ] && echo "Subordinate gid $subgid fail." && exit 2;
 # Needed by rootless:
-		sudo apt install -y dbus-user-session;
-		sudo apt install -y fuse-overlayfs; # recomended
+	sudo apt install -y dbus-user-session;
+	sudo apt install -y fuse-overlayfs; # recomended
 # Rootless docker requires version of slirp4netns greater than v0.4.0 \
 # (when vpnkit is not installed). Check you have this with
 #	slirp4netns --version
 # If you do not have this download and install with \
 #	sudo apt install -y slirp4netns
 # Script to check about this:
-		vp1=`slirp4netns --version | awk '{print $3}' | awk -F "." '{print $1}'`;
-		vp2=`slirp4netns --version | awk '{print $3}' | awk -F "." '{print $2}'`;
-		[ "$vp1" -gt "0" ] && need_install="false" ||
-			[ "$vp2" -ge "4" ] && need_install="false" ||
-				need_install="true";
-		[ "$need_install" = "true" ] && sudo apt install -y slirp4netns;
+	vp1=`slirp4netns --version | awk '{print $3}' | awk -F "." '{print $1}'`;
+	vp2=`slirp4netns --version | awk '{print $3}' | awk -F "." '{print $2}'`;
+	[ "$vp1" -gt "0" ] && need_install="false" ||
+		[ "$vp2" -ge "4" ] && need_install="false" ||
+			need_install="true";
+	[ "$need_install" = "true" ] && sudo apt install -y slirp4netns;
 
 	[ "$reinstall_containerd_docker_ce" = "true" ] && \
 		rm -f ~/.config/systemd/user/docker.service;
@@ -60,14 +60,14 @@ do_docker_stop()
 # as a non-root user to set up the daemon. If this fails, run
 #	sudo apt install -y docker-ce-rootless-extras
 # Script to do so:
-		dockerd-rootless-setuptool.sh install 2>/dev/null || \
-			(sudo apt install -y docker-ce-rootless-extras && \
-				dockerd-rootless-setuptool.sh --force install);
+	dockerd-rootless-setuptool.sh install 2>/dev/null || \
+		(sudo apt install -y docker-ce-rootless-extras && \
+			dockerd-rootless-setuptool.sh --force install);
 # The systemd unit file is installed as ~/.config/systemd/user/docker.service.
-		systemctl --user start docker
-		docker context use rootless
+	systemctl --user start docker
+	docker context use rootless
 
-# For more:
+# For more...
 # - Use the rootless Daemon on your system startup
 # - Client problem (like not having DOCKER_HOST set)
 # - Rootless Docker in Docker
