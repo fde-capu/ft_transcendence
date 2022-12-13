2e227da84bc71ed40b6a6ecd9a6eb7777263ba01
user42 <flavio@carrara.us>
2022-12-13 13:59:02 -0300

    Uninstalled ts-loader, ts-node. Installed typescript 4.9.4 instead. Still scratching head; whatta poor documentation!

--- app_frontend/routes/realtime.ts
+++ app_frontend/routes/realtime.ts
@@ -1,40 +1,47 @@
 //import * as express from "express";
 
+const path = require('path');
+
 const express = require('express');
 //
 ////import * as socketio from "socket.io";
-const path = require('path');
 ////import * as path from "path";
 ////
 const realtime = express();
 
 
 
-//realtime.set("port", process.env.PORT || 3491);
+realtime.set("port", process.env.PORT || 3491);
 ////
-//let http = require("http").Server(realtime);
+let http = require("http").Server(realtime);
 ////// set up socket.io and bind it to our
 ////// http server.
-//let io = require("socket.io")(http);
+let io = require("socket.io")(http);
 ////.listen(http, { resource: '/realtime' });
 ////
 realtime.get('/', async function(req, res, next) {
 	res.sendFile(path.resolve("./public/index.html"));
 //	res.send('Realtime page goes here.');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
 ////
-////// whenever a user connects on port 3000 via
+////// whenever a user connects on port 3491 via
 ////// a websocket, log that a user has connected
 ////io.on("connection", async function(socket) {
 ////  console.log("a user connected");
 ////});
 ////
-//let port = realtime.get("port");
-//const server = realtime.listen(port, function() {
-//    var host = server.address().address;
-//    var port = server.address().port;
-//    console.log("Realtime listening on " + host + " " + port);
-//});
+let port = realtime.get("port");
+const server = realtime.listen(port, function() {
+    var host = express.address().address;
+    var port = express.address().port;
+    console.log("Realtime listening on " + host + " " + port);
+});
 //
 ////////////////////
 //
