1a29c33404eac381f2598edcb5cd35d59010d60c
Flavio Carrara De Capua <flavio@carrara.us>
2022-12-27 21:52:58 -0300

    Added route for authenticator component.

--- app_frontend/src/app/app-routing.module.ts
+++ app_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FttAuthenticatorComponent } from './ftt-authenticator/ftt-authenticator.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+	path: "login",
+	component: FttAuthenticatorComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
