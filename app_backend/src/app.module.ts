2d19ff13ea6c84dc2c400df70562097471b2943b
jhonatan229 <jhonatan229@outlook.com>
2023-01-06 17:05:50 -0300

    configurate and crating tests for controller and service, config enviroments variables

--- app_backend/src/app.module.ts
+++ app_backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { RegisterController } from './user/controller/registred.controller';
 import { UserService } from './user/service/user.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [RegisterController],
   providers: [UserService],
 })
--- app_backend/src/user/service/user.service.ts
+++ app_backend/src/user/service/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { env } from 'process';
 
 @Injectable()
 export class UserService {
@@ -8,14 +7,11 @@ export class UserService {
     async registerUser(code: string){
         const responseToken = await this.httpService.axiosRef.post('https://api.intra.42.fr/oauth/token', {
             grant_type: 'authorization_code',
-            client_id: 'u-s4t2ud-b02a235b40b43fcc4c00653871275c9f93069a8a1b6248f14c516166bfe94f6ax',
-            client_secret: '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x',
+            client_id: process.env.api42_client_id,
+            client_secret: process.env.api42_client_secret,
             redirect_uri: 'http://localhost:3000/user/register',
             code
         });
-        // if (responseToken){
-
-        // }
         return responseToken.data;
     }
 }
