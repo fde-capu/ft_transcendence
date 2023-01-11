9f7e920b1798e5a86b580337639764d260aab9d4
Flavio Carrara De Capua <flavio@carrara.us>
2023-01-11 14:20:57 -0300

    Added relative paths to c_dataclean.sh

--- tools/c_dataclean.sh
+++ tools/c_dataclean.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo rm -rf ./database/data/
+sudo rm -rf ../database/data/
 
-sudo rm -rf ./backend/dist
-sudo rm -rf ./backend/node_modules
+sudo rm -rf ../backend/dist
+sudo rm -rf ../backend/node_modules
 
-sudo rm -rf ./frontend/dist
-sudo rm -rf ./frontend/node_modules
-sudo rm -rf ./frontend/.angular
+sudo rm -rf ../frontend/dist
+sudo rm -rf ../frontend/node_modules
+sudo rm -rf ../frontend/.angular
