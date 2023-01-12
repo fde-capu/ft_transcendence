b86c30d948eba3db63055fbcd1628261c30c04b1
jhonatan229 <jhonatan229@outlook.com>
2023-01-11 21:35:34 -0300

    pass login intra to game page by query

--- frontend/src/app/ftt-authenticator/ftt-authenticator.component.ts
+++ frontend/src/app/ftt-authenticator/ftt-authenticator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AppRoutingModule } from '../app-routing.module';
+import { Router }   from '@angular/router';
 
 @Component({
   selector: 'login',
@@ -7,16 +7,17 @@ import { AppRoutingModule } from '../app-routing.module';
   styleUrls: ['./ftt-authenticator.component.css'],
 })
 export class FttAuthenticatorComponent {
-  constructor(private routes: AppRoutingModule){}
+  constructor(private routes: Router){}
   ngOnInit() {
-    console.log(document.cookie);
     if(document.cookie.split('=')[1])
-    this.routes.navigate(
-      ['/products'],
-      { queryParams: { document.cookie.split('=')[0]: document.cookie.split('=')[1] } }
-    );
-    
-    window.location.href =
+    {
+      this.routes.navigate(
+        ['/game/'],
+        { queryParams: { acess_token: document.cookie.split('=')[1] } }
+        );
+    }
+    else
+      window.location.href =
       'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-b02a235b40b43fcc4c00653871275c9f93069a8a1b6248f14c516166bfe94f6a&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fuser%2Fregister&response_type=code';
   }
 
--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
-export class GameComponent {}
+export class GameComponent {
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const name = this.route.snapshot.queryParamMap.get('acess_token');
+    console.log(name);
+  }
+}
