269e7ddb1ed561e1c3ef8207014c3d996c7f8784
jhonatan229 <jhonatan229@outlook.com>
2023-01-24 12:36:38 -0300

    adjust user controler, take of some rules and put on service user. throw a bad request on service user when the code is empty

--- backend/src/user/controller/registred.controller.ts
+++ backend/src/user/controller/registred.controller.ts
@@ -6,27 +6,20 @@ import { Response } from 'express';
 export class RegisterController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  async code(
+  async registerUser(
     @Query('code') code: string,
     @Res() response: Response = null,
   ): Promise<any> {
-    if (!code || code === '')
-      return response.status(400).json({
-        error: 'Propoer code failed by API42.',
-      });
     try {
-      const tokenFrom42 = await this.userService.getToken(code);
-      const me = await this.userService.getMe(tokenFrom42);
-	  // me = transform me into user_book dto. TODO
-	  const transcendToken = me.login + '_TOKENIZED';
+      const basicUser = await this.userService.registerUser(code);
       return response
-        .cookie('access_token', transcendToken)
-        .cookie('intra_id', me.login)
+        .cookie('access_token', basicUser.token)
+        .cookie('intra_id', basicUser.login)
         .redirect('http://localhost:4200/game');
     } catch (e) {
       console.log('RegisterController failed to generate unique token.');
-      console.log(e.response.data);
-      response.status(e.response.status).json(e.response.data);
+      console.log(e);
+      response.status(e.status).json(e.data);
     }
   }
 }
