117ad627ac9b879ce654a57f70d6b663e1fc2f9d
jhonatan229 <jhonatan229@outlook.com>
2023-01-24 17:45:52 -0300

    creating config to connect on database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,29 @@
 import { HttpModule } from '@nestjs/axios';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { TokenParserMiddleware } from './auth/middleware/token-parser.middleware';
-import { FortyTwoModule } from './forty-two/forty-two.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PingController } from './ping/ping.controller';
-import { RegisterController } from './user/controller/registred.controller';
-import { UserService } from './user/service/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    HttpModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule,
-    FortyTwoModule,
-  ],
-  controllers: [RegisterController, PingController],
-  providers: [UserService],
+  imports: [UserModule, HttpModule, ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),],
+  controllers: [PingController],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
