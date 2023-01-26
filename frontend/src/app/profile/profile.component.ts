032e53e1ca650d0641164861a241f0f4db0a5cd4
Flavio Carrara De Capua <flavio@carrara.us>
2023-01-26 15:05:20 -0300

    User profile extended with style and fancy button behavior. Not yet linked to real data. Some unused functions (that might be global sometime).

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -8,9 +8,52 @@ import { USERS } from '../mocks';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+	// TODO get user value from URI *or* if URI is empty, user is logged user.
 	user: User = {
 		intraId: "ftt_undef_intraId",
 		name: "ftt_undef_name",
 		image: "ftt_undef_image"
 	};
+	focusOn(el: string): void {
+		const exist = document.getElementById(el);
+		if (exist) exist.focus();
+	};
+	focusIf(me: string, cond: string, target: string)
+	{
+		const me_exist = document.getElementById(me);
+		if (!me_exist) return ;
+		const target_exist = document.getElementById(target);
+		if (!target_exist) return ;
+		if (me_exist.innerHTML === cond)
+			target_exist.focus();
+	};
+	switchHTML(el: string, a: string, b: string) {
+		const exist = document.getElementById(el);
+		if (!exist) return ;
+		exist.innerHTML = exist.innerHTML == a ? b : a;
+	}
+	switchIf(el: string, a: string, b: string) {
+		const exist = document.getElementById(el);
+		if (!exist) return ;
+		let n: ReturnType<typeof setTimeout>;
+		n = setTimeout(function() {
+				exist.innerHTML = exist.innerHTML == a ? b : a;
+				}, 300);
+	};
+	blur(el: string)
+	{
+		const exist = document.getElementById(el);
+		if (!exist) return ;
+		exist.blur();
+	}
+	blink(el: string)
+	{
+		const exist = document.getElementById(el);
+		if (!exist) return ;
+		exist.classList.add('inverted');
+		let n: ReturnType<typeof setTimeout>;
+		n = setTimeout(function() {
+				exist.classList.remove('inverted');
+				}, 200);
+	}
 }
