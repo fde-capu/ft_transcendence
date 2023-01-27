57bbddc4435a019b1b5c123355128f65b83f7622
mateus3009 <mateus3009@gmail.com>
2023-01-27 15:19:12 -0300

    feat: new game module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { RegisterController } from './user/controller/registred.controller';
 import { UserService } from './user/service/user.service';
 import { ConfigModule } from '@nestjs/config';
 import { PingController } from './ping/ping.controller';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot()],
+  imports: [HttpModule, ConfigModule.forRoot(), GameModule],
   controllers: [RegisterController, PingController],
   providers: [UserService],
 })
