44c78625b4acd02e00b4f173c82ae9810a32180f
Flavio Carrara De Capua <flavio@carrara.us>
2023-02-10 16:26:58 -0300

    Trying to link the server. BROKEN. Merging everything thats good.

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,6 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { User } from './user';
 import { USERS } from './mocks';
 
@@ -11,8 +12,11 @@ import { USERS } from './mocks';
 })
 export class UserService {
 	loggedUser!: User;
+	private usersUrl = 'http://localhost:3000/user';
 
-	constructor(private route: ActivatedRoute) {
+	constructor(
+		private http: HttpClient
+	) {
 		this.loggedUser = USERS[Math.floor(Math.random() * USERS.length)];
 	}
 
@@ -20,8 +24,13 @@ export class UserService {
 	}
 
 	getOnlineUsers(): Observable<User[]> {
-		const users = USERS;
-		return of(users);
+//		const users = USERS;
+//		return of(users);
+		return this.http.get<User[]>(this.usersUrl)
+			.pipe(
+				tap(_ => console.log(_)),
+				catchError(this.handleError<User[]>('getOnlineUsers', []))
+			);
 	}
 
 	getLoggedUser(): Observable<User> {
@@ -36,7 +45,27 @@ export class UserService {
 		return of(user);
 	}
 
+	getUser(id: string): Observable<User> {
+		const url = `${this.usersUrl}/${id}`;
+		return this.http.get<User>(url).pipe
+		(
+			tap(_ => console.log(`Got user id=${id}`)),
+			catchError(this.handleError<User>(`getUser id=${id}`))
+		);
+	}
+
 	isFriend(user: User): Boolean {
 		return Math.random() > .6; // TODO: Unmock.
+	}
+
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+
+			// TODO: send the error to remote logging infrastructure
+			console.error(error); // log to console instead
+
+			// Let the app keep running by returning an empty result.
+			return of(result as T);
+		};
 	}
 }
