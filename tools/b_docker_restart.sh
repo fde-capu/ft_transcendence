7008681d36b7a66bcce5c82c85974e1c000225e1
Flavio Carrara De Capua <flavio@carrara.us>
2023-02-15 15:34:18 -0300

    b_docker_restart now restarts rootless or default accordingly.

--- tools/b_docker_restart.sh
+++ tools/b_docker_restart.sh
@@ -5,6 +5,11 @@ MYDIR="${MYSELF%/*}";
 FTTDIR="$MYDIR/..";
 
 $MYDIR/b_docker_disable.sh;
-systemctl --user start docker;
-docker context use rootless;
-systemctl --user start docker;
+
+CONTEXT=$(docker info 2>/dev/null | grep Context | sed -e "s/.* //");
+if [ "$CONTEXT" = "rootless" ] ; then
+	systemctl --user start docker;
+	docker context use rootless;
+else
+	systemctl start docker;
+fi;
