2d2fd1b4fd9bf0e1ab3a5a3d6fe7eb1de1c1bead
Flavio Carrara De Capua <flavio@carrara.us>
2023-02-22 13:09:44 -0300

    Disabled rootless kit install.

--- tools/b_docker_restart.sh
+++ tools/b_docker_restart.sh
@@ -6,10 +6,5 @@ FTTDIR="$MYDIR/..";
 
 $MYDIR/b_docker_disable.sh;
 
-CONTEXT=$(docker info 2>/dev/null | grep Context | sed -e "s/.* //");
-if [ "$CONTEXT" = "rootless" ] ; then
-	systemctl --user start docker;
-	docker context use rootless;
-else
-	systemctl start docker;
-fi;
+systemctl stop docker
+systemctl start docker
