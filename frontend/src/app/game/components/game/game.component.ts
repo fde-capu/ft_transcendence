bacb9c14e01edb544e42da20ee1473292c895503
mateus3009 <mateus3009@gmail.com>
2023-03-08 18:38:29 -0300

    adding canvas game

--- frontend/src/app/game/components/game/game.component.ts
+++ frontend/src/app/game/components/game/game.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Single } from './engine';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
-export class GameComponent {}
+export class GameComponent implements AfterViewInit {
+  @ViewChild('game')
+  canvas!: ElementRef<HTMLCanvasElement>;
+
+  running = false;
+
+  ngAfterViewInit(): void {
+    const game = new Single(this.canvas.nativeElement);
+    game.reset();
+    const frameRate = 1000 / 60;
+    let lastUpdate = Date.now();
+    function render() {
+      const currentTimestamp = Date.now();
+      game.update((currentTimestamp - lastUpdate) / 1000);
+      lastUpdate = currentTimestamp;
+      setTimeout(() => {
+        window.requestAnimationFrame(render);
+      }, frameRate);
+    }
+    window.requestAnimationFrame(render);
+  }
+}
