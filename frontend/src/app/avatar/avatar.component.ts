a385dd1aa581258d7fb2627523b3546faad31a59
Flavio Carrara De Capua <flavio@carrara.us>
2023-03-11 20:39:26 -0300

    ...slight trickier, also avatar updates.

--- frontend/src/app/avatar/avatar.component.ts
+++ frontend/src/app/avatar/avatar.component.ts
@@ -10,6 +10,7 @@ styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent {
 	@Input() user?: User;
+	displayUser?: User;
 	popUpOn = false;
 	isFriend: boolean = false;
 	isMe: boolean = false;
@@ -20,16 +21,35 @@ export class AvatarComponent {
 		private readonly invitationService: InvitationService,
 	){}
 
+	ngOnChanges() {
+		this.checkFriendship();
+		this.checkMe();
+		this.updateMe();
+	}
+
+	async updateMe() {
+		if (!this.user)
+		{
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			await this.updateMe();
+		}
+		else // Because no `return` above, so have to trick TS.
+		{
+			this.userService.getUser(this.user.intraId).subscribe(_=>{
+				this.displayUser = _;
+			});
+			// Lazy update every 15 seconds, because there are many instances of avatars.
+			// Note: chat messages do not update retroactively, but take changes from point on.
+			await new Promise(resolve => setTimeout(resolve, 15000));
+			await this.updateMe();
+		}
+	}
+
 	checkMe() {
 		this.userService.getLoggedUser().subscribe(_=>{
 			this.loggedUser = _;
 			this.isMe = _.intraId == this.user?.intraId;
 		});
-	}
-
-	ngOnChanges() {
-		this.checkFriendship();
-		this.checkMe();
 	}
 
 	inviteToChat() {
